bb696982a2e50d9b8e7fc75fbb0fc3d90c9065a7
KathiWagner <kwagner@mail.hs-ulm.de>
2018-11-16 11:01:13 +0100

    measurement_programm changed

--- measurement_program.py
+++ measurement_program.py
@@ -1,29 +1,21 @@
 #!/usr/bin/env python
 
 import random
-#import matplotlib.pyplot as plt
-
+import time
 
 data = open('/root/lmic_pi/examples/grab-and-send/measurement.txt', "w")
 people = []
 people_hex =[]
 time_sec= [0,60,120,180,240, 300, 360]#range(0,240,60) # in sec
 for x in range(len(time_sec)):
+    data = open('/root/lmic_pi/examples/grab-and-send/measurement.txt', "w")
     a = random.randint(1,201)
-    people.append(a)
-    people_hex.append(hex(a))
-    b= str(time_sec[x])+ ' ' + str(a)
     data.write(str(time_sec[x]))
     data.write(' ')
     data.write(hex(a))
-    data.write('\n')
+    data.close()
+    time.sleep(1)
 
-data.close()
-
-#print(time_sec,people)
-#print(people_hex)
-#plt.plot(time_sec,people)
-#plt.show()
 
 
 
