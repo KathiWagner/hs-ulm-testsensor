2543d06d1e4f8a531087a25410b206ad5d1cbc7c
KathiWagner <kwagner@mail.hs-ulm.de>
2019-02-21 07:40:57 +0100

    Sensor zeigt nun immer die Zahl 100 an.

--- measurement_program.py
+++ measurement_program.py
@@ -9,7 +9,8 @@ people_hex =[]
 time_sec= [0,60,120,180,240, 300, 360]#range(0,240,60) # in sec
 for x in range(len(time_sec)):
     data = open('/root/lmic_pi/examples/grab-and-send/measurement.txt', "w")
-    a = random.randint(1,201)
+    #a = random.randint(1,201)
+    a = 100
     data.write(str(time_sec[x]))
     data.write(' ')
     data.write(hex(a))
